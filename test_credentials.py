dbc2a036d1236f75ce903dad687adbd471a2e9e1
Sean Summers <sean.summers@apexon.com>
2022-08-24 13:46:15 -0400

    add test_credentials coverage

--- test_credentials.py
+++ test_credentials.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 
 from credentials import CredentialMac
@@ -10,6 +11,17 @@ class TestCredentialMac(unittest.TestCase):
             expirationDate="2003-01-15T11:42:46-08:00",
         )
         self.assertEqual(credential_mac.mac(), "cR6Jpz58nriXERDN")
+
+    def test_mac_datetime(self):
+        class TestTZInfo(datetime.tzinfo):
+            def utcoffset(self, dt):
+                return datetime.timedelta(hours=-8)
+
+        credential_mac = CredentialMac(
+            creationDate=datetime.datetime(2003, 1, 15, 8, 42, 46, tzinfo=TestTZInfo()),
+            expirationDate=datetime.datetime(2003, 1, 15, 11, 42, 46, tzinfo=TestTZInfo()),
+        )
+        self.assertEqual(credential_mac.mac("password"), "5KMM/p4KtK5oAf/B")
 
     def test_mac_password(self):
         credential_mac = CredentialMac(
